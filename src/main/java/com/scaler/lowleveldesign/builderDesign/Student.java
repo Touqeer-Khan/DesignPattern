e086f98d627ea5818445ad2500cf89650533b5f6
Tk12-th <touqeerkhan678@gmail.com>
2025-11-08 01:05:39 +0530

    ProductionReadyBuilder

--- src/main/java/com/scaler/lowleveldesign/builderDesign/Student.java
+++ src/main/java/com/scaler/lowleveldesign/builderDesign/Student.java
@@ -30,56 +30,93 @@ public class Student {
         this.avg_psp=builder.getAvg_psp();
 
     }
+    public static  Builder getBuilder(){
+        return new Builder();
+    }
 
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public String getLastName() {
-        return lastName;
 
-    }
-    public void setLastName(String lastName) {
 
-        this.lastName = lastName;
-    }
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age) {
-        this.age = age;
-    }
-    public String getGender() {
-        return gender;
-    }
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-    public String getAddress() {
-        return address;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    public double getHeight() {
-        return height;
-    }
-    public void setHeight(double height) {
-        this.height = height;
-    }
-    public String getUniversity() {
-        return university;
-    }
-    public void setUniversity(String university) {
-        this.university = university;
-    }
-    public float getAvg_psp() {
-        return avg_psp;
-    }
-    public void setAvg_psp(float avg_psp) {
-        this.avg_psp = avg_psp;
+    //Inner Class----A class within the class
+    public static class Builder {
+
+        private String firstName;
+        private String lastName;
+        private int age;
+        private String gender;
+        private String address;
+        private double height;
+        private String university;
+        private float avg_psp;
+
+        public String getFirstName() {
+            return firstName;
+        }
+        public String getLastName() {
+            return lastName;
+        }
+        public int getAge() {
+            return age;
+
+        }
+        public String getGender() {
+            return gender;
+        }
+        public String getAddress() {
+            return address;
+        }
+        public double getHeight() {
+            return height;
+        }
+        public String getUniversity() {
+            return university;
+        }
+        public float getAvg_psp() {
+            return avg_psp;
+        }
+
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setAvg_psp(float avg_psp) {
+            this.avg_psp = avg_psp;
+            return this;
+        }
+
+        public Builder setUniversity(String university) {
+            this.university = university;
+            return this;
+        }
+
+        public Builder setHeight(double height) {
+            this.height = height;
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder setGender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+        public Student build(){
+            return new Student(this);
+        }
     }
 
 
--- src/main/java/com/scaler/lowleveldesign/builderDesign/client.java
+++ src/main/java/com/scaler/lowleveldesign/builderDesign/client.java
@@ -4,14 +4,26 @@ public class client {
 
     public static void main(String[] args) {
 
-        Builder builder = new Builder();
-        builder.setAge(25);
-        builder.setHeight(150);
-        builder.setGender("male");
-        builder.setFirstName("John");
-
-        Student student=new Student(builder);
-
+//        Builder builder = new Builder();
+//        Student.Builder builder=Student.getBuilder();
+//        builder.setAge(25);
+//        builder.setHeight(150);
+//        builder.setGender("male");
+//        builder.setFirstName("John");
+//
+//        Student student=Builder.build();
+        Student student=Student.getBuilder()
+                        .setFirstName("John")
+                        .setAge(25)
+                        .setHeight(150)
+                        .setGender("male")
+                        .build();
+        Student student1=Student.getBuilder()
+                .setFirstName("tom")
+                .setAge(26)
+                .setHeight(167)
+                .setGender("male")
+                .build();
         System.out.println(student);
     }
 }
